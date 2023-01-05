ec14a75d9b9bfa3255a31bbaa2f5bb1dd8c02f43
lucastusch <lucas.tusch@gmail.com>
2023-01-05 19:51:51 +0100

    building hero
    still in progress ("play now")

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,8 @@ import unologo from '../assets/uno-logo.png'
 const Navbar = () => {
   return (
     <>
-      <section className=''>
-        <div className='w-screen h-20 z-10 border-b-2 drop-shadow-lg bg-gray-100'>
+      <header>
+        <div className='w-screen h-20 z-10 bg-white'>
           <div className='px-2 flex justify-between items-center w-full h-full'>
             <div className='flex items-center mx-40'>
               <img src={unologo} alt='logo' className='h-16 w-24' />{' '}
@@ -16,7 +16,7 @@ const Navbar = () => {
               </div>
             </div>
             <div className='pr-40'>
-              <button className='font-semibold px-5 py-2 rounded-md  text-blue-500 hover:text-blue-600 bg-slate-200 hover:bg-slate-300 drop-shadow-md hover:drop-shadow-lg duration-300 bg-transparent mr-4'>
+              <button className='font-semibold px-5 py-2 rounded-md  text-blue-500 bg-slate-200 hover:text-white hover:bg-blue-500 duration-500 drop-shadow-md hover:drop-shadow-lg  bg-transparent mr-4'>
                 <Link to='/signin'>Sign In</Link>
               </button>
               <button className='font-semibold px-5 py-2 rounded-md text-white bg-blue-500 hover:bg-blue-600 drop-shadow-md hover:drop-shadow-lg duration-300 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300'>
@@ -25,7 +25,7 @@ const Navbar = () => {
             </div>
           </div>
         </div>
-      </section>
+      </header>
     </>
   )
 }
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,12 +1,14 @@
 //import { useState } from 'react'
 //import { Link } from 'react-router-dom'
+import unoongoinggame from '../assets/uno-ongoing-game.jpg'
 
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
 const Landing = () => {
-  //const [lobbyid, setLobbyID] = useState(1)
   /*
+  const [lobbyid, setLobbyID] = useState(1)
+  
   //same function as the clickhandler below
   const clickHandler = () => {
     setLobbyID((prevState) => {
@@ -14,17 +16,17 @@ const Landing = () => {
       return prevState
     })
   }
-*/
-  /*
+
+  
   //same function as the clickhandler above
   const clickHandler = () => {
     setLobbyID(lobbyid + 1)
   }
-*/
+
 
   //<Link to={`/game/${lobbyid}`}>Lobby-{lobbyid}</Link>
 
-  /*
+  
    <h1 className='text-3xl font-bold mb-10'>Welcome to !UNO</h1>
       <h2 className='text-2xl font-bold'>Server-List</h2>
       <ol className='list-disc'>
@@ -38,15 +40,53 @@ const Landing = () => {
           </button>
         </li>
       </ol>
-*/
+  */
 
   return (
     <>
-      <section>
-        <Navbar />
-        <div>Hello World</div>
-        <Footer />
-      </section>
+      <Navbar />
+      <main>
+        <div className='text-center relative bg-white overflow-hidden'>
+          <div className='max-w-7xl mx-auto'>
+            <div className='pt-16 relative z-10 bg-white sm:pb-10 md:pb-10 lg:max-w-2xl lg:w-full lg:pb-14 xl:pb-40'>
+              <svg
+                className='text-left hidden lg:block absolute right-[-10px] bot-[-150px] inset-y-0 text-white transform translate-x-1/2'
+                fill='currentColor'
+                viewBox='0 0 100 100'
+                preserveAspectRatio='none'
+                aria-hidden='true'
+                height='680px'
+                width='250px'
+              >
+                <polygon points='45,0 100,0 40,100 0,100' />
+              </svg>
+              <article className='mb-20 md:mb-20 lg:mb-40  mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-10 lg:mt-10 lg:px-0 xl:mt-28'>
+                <div className='sm:text-center lg:text-left'>
+                  <h1 className='text-3xl tracking-tight font-extrabold sm:text-5xl md:text-6xl'>
+                    <span className='block text-red-500 drop-shadow-lg xl:inline'>
+                      PLAY UNO TOGETHER
+                    </span>{' '}
+                    <br />
+                    <span className='block text-yellow-500 drop-shadow-lg xl:inline'>
+                      WITH YOUR FRIENDS
+                    </span>
+                    <br />
+                    <button className='py-3 px-2 bg-green-300 rounded-md font-bold text-2xl tracking-tighter shadow-lg hover:shadow-xl scale-90 hover:scale-100 duration-300'>
+                      <span className='text-white'>PLAY NOW</span>
+                    </button>
+                  </h1>
+                </div>
+              </article>
+            </div>
+          </div>
+          <div className='pb-16 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2'>
+            <div>
+              <img src={unoongoinggame} alt='ongoing uno game' />
+            </div>
+          </div>
+        </div>
+      </main>
+      <Footer />
     </>
   )
 }
